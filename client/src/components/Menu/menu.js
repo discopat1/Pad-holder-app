6f0e4a5e3d0158c8a2b6bb9c257d447eb6ec9609
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-11-07 17:58:17 -0700

    menu page styled

--- client/src/components/Menu/menu.js
+++ client/src/components/Menu/menu.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import "./menu.css"
 
 
-const Menu = () => (
-    <div>
-    <button>Start</button>
+class Menu extends Component {
+
+    state = {
+        level: "",
+        style: "",
+    }
+
+    
+    render() {
+        return (
+    <div class="bg-dark">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                            <img src={"./images/senchai-pads.jpg"} className="rounded float-right image" alt="..."/>
+
+                <h1 class="display-4">Virtual Pad Holder!</h1>
+                <p class="lead text-danger">Increase your reaction time as classic fight combinations are called out to you!</p>
+            </div>
+        </div>
+        <br/>
+        <div className="row justify-content-around">
+            <div className="col-4">
+                <button class="text-danger">Beginner</button>
+            </div>
+            <div className="col-4">
+                <button class="text-danger">Boxing</button>
+            </div>
+        </div>
+        <br/>
+        <div className="row justify-content-around">
+            <div className="col-4">
+                <button class="text-danger">Intermediate</button>
+            </div>
+            <div className="col-4">
+                <button class="text-danger">Kickboxing</button>
+            </div>
+        </div>
+        <br/>
+        <div className="row justify-content-around">
+            <div className="col-4">
+                <button class="text-danger">Advanced</button>
+                <br/>
+                <br/>
+                <button type="button" class="bg-success text-light">Start</button>
+            </div>
+            <div className="col-4">
+                <button class="text-danger">MMA</button>
+            </div>
+        </div>
     </div>
-);
+        );
+    };
+};
+    
 
 export default Menu;
