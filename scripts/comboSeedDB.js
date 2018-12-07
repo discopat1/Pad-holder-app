ec092b2fe3ac1c68f0af6bd9aab80c6d99ea5283
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-12-06 18:17:18 -0700

    added some combos

--- scripts/comboSeedDB.js
+++ scripts/comboSeedDB.js
@@ -10,109 +10,193 @@ mongoose.connect(
 
 const comboSeed = [
     {
-        combo: ["Jab"],
+        combo: "Jab",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross"],
+        combo: "Cross",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, hook"],
+        combo: "High-hook",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, hook, cross"],
+        combo: "Low-hook",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, hook, cross, uppercut"],
+        combo: "Uppercut",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, round-kick"],
+        combo: "Teep",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, round-kick"],
+        combo: "Round-kick",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, hook, round-kick"],
+        combo: "Switch-kick",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, hook, cross, round-kick"],
+        combo: "Spinning back-kick",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Jab, cross, hook, cross, uppercut, round-kick"],
+        combo: "Spinning hook-kick",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["Superman jab, round-kick"],
-        boxing: false,
-        kickboxing: true,
-        mma: true
-    },
-    {
-        combo: ["Cross, switch-kick"],
-        boxing: false,
-        kickboxing: true,
-        mma: true
-    },
-    {
-        combo: ["jab, overhand"],
+        combo: "Jab, cross",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["jab, cross, uppercut, cross"],
+        combo: "Jab, cross, hook",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["double jab, cross, low-hook, high-hook"],
+        combo: "Jab, cross, hook, cross",
         boxing: true,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["cross, hook, round-kick"],
+        combo: "Jab, cross, hook, cross, uppercut",
+        boxing: true,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Jab, round-kick",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["teep, cross, high-hook"],
+        combo: "Jab, cross, round-kick",
         boxing: false,
         kickboxing: true,
         mma: true
     },
     {
-        combo: ["cross, hook, cross, round-kick"],
+        combo: "Jab, cross, switch-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Jab, cross, hook, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Jab, cross, hook, cross, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Jab, cross, hook, cross, switch-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Jab, cross, hook, cross, uppercut, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Superman jab, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Cross, switch-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "Jab, overhand",
+        boxing: true,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "jab, cross, uppercut, cross",
+        boxing: true,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "double jab, cross, low-hook, high-hook",
+        boxing: true,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "double jab, cross, low-hook, high-hook, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "cross, hook, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "cross, upper-cut, round-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "teep, cross, high-hook",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "cross, hook, cross, switch-kick",
+        boxing: false,
+        kickboxing: true,
+        mma: true
+    },
+    {
+        combo: "cross, hook, spinning back-kick",
         boxing: false,
         kickboxing: true,
         mma: true
